ec14e3df894aeb54c8a06333b27d599ba4b3f28b
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-05-29 22:25:51 +0530

    Added Add to Cart to option in smartphones

---
+++ script.js
@@ -0,0 +1,24 @@
+// Add to cart functionality
+
+let addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+
+
+// Function to add items to the cart for xiomi13
+let xiomi13 = document.querySelector('#xiomi13');
+let addToCartXiomi13 = document.querySelector('#atc-xiomi13');
+
+
+let cartFunctionXiomi13 = () => {
+
+    console.log("dhasjhbkjdb");
+    
+window.location.href = 'F:/WEB PROJECTS/Dealmart/AddToCart.html';
+    
+addToCartButtons=addToCartXiomi13
+console.log(addToCartButtons);
+
+
+}
+addToCartXiomi13.addEventListener('click', cartFunctionXiomi13);
+
