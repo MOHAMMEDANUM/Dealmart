5b50f9b6193f0b32593fb8fa19144d12f2d4f3dd
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-06-03 21:46:51 +0530

    Working on Add to Cart functionality for xiomi13

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Add to cart functionality
 
 let addToCartButtons = document.querySelectorAll('.add-to-cart');
-
+let addToCartPrice = document.querySelector("#price-added");
+console.log(addToCartPrice.innerHTML);
 
 
 // Function to add items to the cart for xiomi13
 let xiomi13 = document.querySelector('#xiomi13');
-let addToCartXiomi13 = document.querySelector('#atc-xiomi13');
+let addToCartXiomi13 = document.querySelector('.atc-xiomi13');
+let priceOfXiomi13=document.querySelector("#price");
+console.log(priceOfXiomi13.innerHTML);
+
+
+
+
+
 
 
 let cartFunctionXiomi13 = () => {
@@ -14,6 +22,8 @@ let cartFunctionXiomi13 = () => {
     console.log("dhasjhbkjdb");
     
 window.location.href = 'F:/WEB PROJECTS/Dealmart/AddToCart.html';
+addTOCartPrice.innerHTML=priceOfXiomi13.innerHTML
+
     
 addToCartButtons=addToCartXiomi13
 console.log(addToCartButtons);
